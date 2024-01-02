7431c7ccd6bad7dcba81ff7380620d221b478bac
gabriel <bielh2001@gmail.com>
2024-01-02 07:55:48 -0300

    transferencia de pc

---
+++ superchoque2000/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calc1 = new Calculadora();
+        
+
+
+
+        calc1.perguntas();
+        calc1.calcular();
+
+
+
+
+
+
+
+
+
+    }
+}
